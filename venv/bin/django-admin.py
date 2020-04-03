cc085b3d6d12f47545ebe853645f7c8b0cd4f099
SameeranB <sameeranbandishti@ieee.org>
2020-04-03 10:04:45 +0530

    Initial Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/caronex/Tech/Work/Web-Dev/FullStack/LifestyleForHealth/DialADiet/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
