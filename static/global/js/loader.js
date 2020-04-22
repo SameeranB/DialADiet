ea257a6e18552373d317101ec7c19606b4ef99ec
SameeranB <sameeranbandishti@ieee.org>
2020-04-22 18:36:48 +0530

    Add the loader and concerned assets. Implement the loader on sign up and login form submissions

---
+++ static/global/js/loader.js
@@ -0,0 +1,6 @@
+function activate_loader() {
+    console.log("loader activated")
+    document.getElementById('loader-wrapper').style.display = "none";
+    document.getElementById('svg-wrapper').style.display = "flex";
+
+}
